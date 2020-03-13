540464ad927f264dcddc7a27092f1fd7d4db8985
nalbam <me@nalbam.com>
2020-03-13 15:12:59 +0900

    pulse_duration

--- run.py
+++ run.py
@@ -167,7 +167,7 @@ def main():
                 continue
             pulse_end = time.time()
 
-            pulse_duration = 100  # pulse_end - pulse_start
+            pulse_duration = pulse_end - pulse_start
             distance = pulse_duration * 17000
             distance = round(distance, 2)
 
